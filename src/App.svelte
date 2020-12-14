db9abb325e7de611c178cc90e8984c3e7499688f
Alex Avelar <aleksluciano@gmail.com>
2020-12-14 03:02:05 -0300

    correcao .svelte para componente login

--- src/App.svelte
+++ src/App.svelte
@@ -249,4 +249,4 @@ import Login from './components/Login.svelte';
 <!-- <button on:click={login}> Entrar </button> -->
 <Login on:logUser={(e) => { console.log(e,"jdhsja"); auth.signInWithEmailAndPassword(e.detail.user, e.detail.senha); } }/>
 {/if}
-<!-- 123 -->
+<!-- 1234 -->
